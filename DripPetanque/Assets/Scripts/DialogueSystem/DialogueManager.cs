97e2d784806066a94a631c7d8c90d1cfc906d49e
Victor <victordesenneville@gmail.com>
2023-10-20 16:10:35 +0200

    test push (imported some fonts + visual rework of dialogue container

--- DripPetanque/Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ DripPetanque/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -15,15 +15,48 @@ public class DialogueManager : MonoBehaviour
 {
     public static Dictionary<DialogueIDWraper, EventHandler> EventHandlers = new Dictionary<DialogueIDWraper, EventHandler>();
 
+    //----------------------------------------------------------
+    #region Serialized Fields
+    //----------------------------------------------------------
     [SerializeField] private float m_clickCooldown;
-    [SerializeField] private GameObject m_dialogueContent;
-    [SerializeField] private GameObject m_nextButtonObj;
+    [SerializeField] private GameObject m_dialogueCanvas;
+    [SerializeField] private CanvasGroup m_dialogueMainContainerCanvasGroup;
+    [SerializeField] private GameObject m_nextButtonGFXObj;
     [SerializeField] private TMP_Text m_dialogueText;
-    [SerializeField] private Image m_talkerImage;
+    [SerializeField] private GameObject m_dialogueTextContainer;
+    //[SerializeField] private Image m_talkerImage;
     [SerializeField] private TMP_Text m_talkerNameText;
     [SerializeField] private AnimationCurve m_fadeCurve;
     [SerializeField] private AnimationCurve m_swipeCurve;
 
+    [Header("DoTween Zoom Sequence Reference")]
+    [SerializeField] private Vector2 _minAnchorAnimZoomStart;
+    [SerializeField] private Vector2 _maxAnchorAnimZoomStart;
+    [SerializeField] private Vector2 _minAnchorAnimZoomEnd;
+    [SerializeField] private Vector2 _maxAnchorAnimZoomEnd;
+
+    [Header("DoTween Swipe from down Sequence Reference")]
+    [SerializeField] private Vector2 _minAnchorAnimSwipeDownStart;
+    [SerializeField] private Vector2 _maxAnchorAnimSwipeDownStart;
+    [SerializeField] private Vector2 _minAnchorAnimSwipeDownEnd;
+    [SerializeField] private Vector2 _maxAnchorAnimSwipeDownEnd;
+
+    [Header("DoTween Swipe from left Sequence Reference")]
+    [SerializeField] private Vector2 _minAnchorAnimSwipeLeftStart;
+    [SerializeField] private Vector2 _maxAnchorAnimSwipeLeftStart;
+    [SerializeField] private Vector2 _minAnchorAnimSwipeLeftEnd;
+    [SerializeField] private Vector2 _maxAnchorAnimSwipeLeftEnd;
+
+    [Header("DoTween Swipe from right Sequence Reference")]
+    [SerializeField] private Vector2 _minAnchorAnimSwipeRightStart;
+    [SerializeField] private Vector2 _maxAnchorAnimSwipeRightStart;
+    [SerializeField] private Vector2 _minAnchorAnimSwipeRightEnd;
+    [SerializeField] private Vector2 _maxAnchorAnimSwipeRightEnd;
+    #endregion
+
+    //----------------------------------------------------------
+    #region Private Fields
+    //----------------------------------------------------------
     private DialogueData m_currentDialogueData;
     private List<SentenceData> m_sentenceDatas = new List<SentenceData>();
     private int m_sentenceIndex;
@@ -33,6 +66,8 @@ public class DialogueManager : MonoBehaviour
     private bool m_isClickedOnce;
     private bool m_canClickAgain;
     private TextType m_textType;
+    private Sequence _openDroplistSequence;
+    #endregion
 
 
     public void StartDialogue(DialogueData dialogueData)
@@ -44,6 +79,7 @@ public class DialogueManager : MonoBehaviour
     {
         m_haveFinishDialogue = false;
         m_sentenceIndex = 0;
+        m_dialogueText.text = "";
         m_textType = dialogueData.textType;
         yield return new WaitForEndOfFrame();
         m_sentenceDatas.Clear();
@@ -52,11 +88,18 @@ public class DialogueManager : MonoBehaviour
         {
             m_sentenceDatas.Add(sentenceData);
         }
+
+        //Set up dialogue UI element at first start, then update at each ComputeSentences()
+        if (m_textType == DialogueData.TextType.dialogue)
+        {
+            //m_talkerImage.gameObject.SetActive(true);
+            m_talkerNameText.gameObject.SetActive(true);
+            //m_talkerImage.sprite = m_sentenceDatas[m_sentenceIndex].NPCSprite;
+            m_talkerNameText.text = m_sentenceDatas[m_sentenceIndex].NPCName;
+
+        }
+
         ShowDialogue();
-        //DisplayNextDialogue();
-        ComputeSentences();
-
-
     }
 
     private void ShowDialogue()
@@ -70,18 +113,18 @@ public class DialogueManager : MonoBehaviour
         SentenceData currentSentenceData = m_sentenceDatas[m_sentenceIndex];
         m_dialogueText.text = currentSentenceData.sentence;
 
-        m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
+        //m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
 
         if (m_textType == DialogueData.TextType.dialogue)
         {
-            m_talkerImage.gameObject.SetActive(true);
-            m_talkerNameText.transform.parent.gameObject.SetActive(true);
-            m_talkerImage.sprite = currentSentenceData.NPCSprite;
+            //m_talkerImage.gameObject.SetActive(true);
+            m_talkerNameText.gameObject.SetActive(true);
+            //m_talkerImage.sprite = currentSentenceData.NPCSprite;
             m_talkerNameText.text = currentSentenceData.NPCName;
         }
         else if (m_textType == DialogueData.TextType.text)
         {
-            m_talkerImage.gameObject.SetActive(false);
+            //m_talkerImage.gameObject.SetActive(false);
             m_talkerNameText.gameObject.SetActive(false);
         }
 
@@ -91,6 +134,7 @@ public class DialogueManager : MonoBehaviour
         {
             case DialogueData.SentenceDisplayStyle.direct:
                 m_dialogueText.text = currentSentenceData.sentence;
+                m_nextButtonGFXObj.SetActive(true);
                 break;
 
             case DialogueData.SentenceDisplayStyle.type:
@@ -105,26 +149,21 @@ public class DialogueManager : MonoBehaviour
 
     public void DisplayNextDialogue()
     {
-        if (m_canClickAgain == false)
+        if (m_currentDialogueData.sentenceDatas[m_sentenceIndex].typingIsComplete)
         {
-            if (m_currentDialogueData.sentenceDatas[m_sentenceIndex].typingIsComplete)
-            {
-                m_sentenceIndex++;
-                //feedbackSFXAudioSource.PlayOneShot(m_nextDialogueButtonClickSound);
-                m_isClickedOnce = false;
-            }
+            m_sentenceIndex++;
+            //feedbackSFXAudioSource.PlayOneShot(m_nextDialogueButtonClickSound);
+            m_isClickedOnce = false;
+        }
 
-            if (m_sentenceIndex < m_sentenceDatas.Count)
-            {
-                ComputeSentences();
-            }
-            else
-            {
-                //Callback();
-                EndDialogue();
-            }
-
-            StartCoroutine(ClickCooldownCoroutine());
+        if (m_sentenceIndex < m_sentenceDatas.Count)
+        {
+            ComputeSentences();
+        }
+        else
+        {
+            //Callback();
+            EndDialogue();
         }
 
     }
@@ -147,10 +186,11 @@ public class DialogueManager : MonoBehaviour
         currentSentenceData.typingIsComplete = false;
         m_isSkippable = currentSentenceData.isSkippable;
         m_dialogueText.text = "";
-        if (!m_isSkippable)
-        {
-            m_nextButtonObj.SetActive(false);
-        }
+        m_nextButtonGFXObj.SetActive(false);
+        //if (!m_isSkippable)
+        //{
+        //    m_nextButtonObj.SetActive(false);
+        //}
         if (!m_isClickedOnce)
         {
             m_isClickedOnce = true;
@@ -165,7 +205,8 @@ public class DialogueManager : MonoBehaviour
             m_dialogueText.text = currentSentenceData.sentence;
         }
         currentSentenceData.typingIsComplete = true;
-        m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
+        m_nextButtonGFXObj.SetActive(true);
+        //m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
     }
 
     #region Dialogue Transition in
@@ -176,8 +217,9 @@ public class DialogueManager : MonoBehaviour
         switch (textBoxDisplayStyle)
         {
             case DialogueData.DialogueStartDisplayStyle.direct:
-                m_dialogueContent.SetActive(true);
-                m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
+                m_dialogueCanvas.SetActive(true);
+                ComputeSentences();
+                //m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
                 break;
 
             case DialogueData.DialogueStartDisplayStyle.fade:
@@ -194,59 +236,57 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
+    private void ToggleDialogueInterfaceElements(bool state)
+    {
+        m_dialogueTextContainer.gameObject.SetActive(state);
+        //m_talkerImage.gameObject.SetActive(state);
+        m_talkerNameText.gameObject.SetActive(state);
+    }
+
     private void DialogueFadeIn(float transitionDuration)
     {
-        m_dialogueContent.SetActive(true);
-        Sequence sequence = DOTween.Sequence();
-        sequence.Append(m_dialogueContent.GetComponent<Image>().DOFade(1, transitionDuration).SetEase(m_fadeCurve));
-        sequence.Join(m_dialogueText.GetComponent<TMP_Text>().DOFade(1, transitionDuration).SetEase(m_fadeCurve));
-        if (m_textType == DialogueData.TextType.dialogue)
+        m_dialogueCanvas.SetActive(true);
+        _openDroplistSequence = DOTween.Sequence();
+        _openDroplistSequence.Append(m_dialogueMainContainerCanvasGroup.DOFade(1, transitionDuration).From(0).SetEase(m_fadeCurve));
+
+        _openDroplistSequence.OnComplete(() =>
         {
-            sequence.Join(m_talkerImage.GetComponent<Image>().DOFade(1, transitionDuration).SetEase(m_fadeCurve));
-            sequence.Join(m_talkerNameText.GetComponent<TMP_Text>().DOFade(1, transitionDuration).SetEase(m_fadeCurve));
-        }
-        sequence.OnComplete(() =>
-        {
-            m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
+            ComputeSentences();
         });
-        sequence.Play();
+        _openDroplistSequence.Play();
     }
 
     private void DialogueZoomIn(float transitionDuration)
     {
-        Vector2 originPos = m_dialogueContent.GetComponent<RectTransform>().localScale;
-        m_dialogueContent.GetComponent<RectTransform>().localScale = new Vector2(0, 0);
-        m_dialogueContent.SetActive(true);
-        showNPCImage(true);
+        m_dialogueCanvas.SetActive(true);
 
-        Sequence sequence = DOTween.Sequence();
+        _openDroplistSequence = DOTween.Sequence();
+        _openDroplistSequence.Insert(0f, m_dialogueMainContainerCanvasGroup.GetComponent<RectTransform>().DOAnchorMin(_minAnchorAnimZoomEnd, transitionDuration).From(_minAnchorAnimZoomStart).SetEase(Ease.OutQuart));
+        _openDroplistSequence.Insert(0f, m_dialogueMainContainerCanvasGroup.GetComponent<RectTransform>().DOAnchorMax(_maxAnchorAnimZoomEnd, transitionDuration).From(_maxAnchorAnimZoomStart).SetEase(Ease.OutQuart));
 
-        sequence.Append(m_dialogueContent.GetComponent<RectTransform>().DOScale(originPos, transitionDuration).SetEase(m_fadeCurve));
-        sequence.OnComplete(() =>
+        _openDroplistSequence.OnComplete(() =>
         {
-            m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
+            ToggleDialogueInterfaceElements(true);
+            ComputeSentences();
         });
-        sequence.Play();
+
+        _openDroplistSequence.Play();
     }
 
     private void DialogueSwipIn(float transitionDuration)
     {
         DialogueData.SwipInDirection swipInDirection = m_currentDialogueData.swipInDirection;
-        m_dialogueContent.SetActive(true);
-        showNPCImage(true);
+        m_dialogueCanvas.SetActive(true);
+
+        RectTransform dialogueContainerRectTransform = m_dialogueMainContainerCanvasGroup.GetComponent<RectTransform>();
 
         Vector2 originPos;
-        originPos.y = m_dialogueContent.transform.position.y;
-        originPos.x = m_dialogueContent.transform.position.x;
+        originPos.y = dialogueContainerRectTransform.localPosition.y;
+        originPos.x = dialogueContainerRectTransform.localPosition.x;
         Vector2 tempPos = originPos;
 
         switch (swipInDirection)
         {
-            /*
-            case DialogueData.SwipInDirection.fromUp:
-                tempPos.y += Screen.height;
-                break;
-            */
             case DialogueData.SwipInDirection.fromDown:
                 tempPos.y -= (Screen.height / 2) + tempPos.y / 2;
                 break;
@@ -259,19 +299,19 @@ public class DialogueManager : MonoBehaviour
                 tempPos.x += Screen.width;
                 break;
         }
-        m_dialogueContent.transform.position = tempPos;
+        dialogueContainerRectTransform.localPosition = tempPos;
         Sequence sequence = DOTween.Sequence();
         if (/*swipInDirection == DialogueData.SwipInDirection.fromUp || */ swipInDirection == DialogueData.SwipInDirection.fromDown)
         {
-            sequence.Append(m_dialogueContent.transform.DOMoveY(originPos.y, transitionDuration).SetEase(m_swipeCurve));
+            sequence.Append(dialogueContainerRectTransform.DOMoveY(originPos.y, transitionDuration).SetEase(m_swipeCurve));
         }
         else if (swipInDirection == DialogueData.SwipInDirection.fromLeft || swipInDirection == DialogueData.SwipInDirection.fromRight)
         {
-            sequence.Append(m_dialogueContent.transform.DOMoveX(originPos.x, transitionDuration).SetEase(m_swipeCurve));
+            sequence.Append(dialogueContainerRectTransform.DOMoveX(originPos.x, transitionDuration).SetEase(m_swipeCurve));
         }
         sequence.OnComplete(() =>
         {
-            m_nextButtonObj.SetActive(m_sentenceIndex != m_sentenceDatas.Count - 1);
+            ComputeSentences();
         });
         sequence.Play();
     }
@@ -285,8 +325,8 @@ public class DialogueManager : MonoBehaviour
         switch (textBoxDisplayStyle)
         {
             case DialogueData.DialogueEndDisplayStyle.direct:
-                m_dialogueContent.SetActive(false);
-                m_nextButtonObj.SetActive(false);
+                m_dialogueCanvas.SetActive(false);
+                m_nextButtonGFXObj.SetActive(false);
                 m_haveFinishDialogue = true;
                 break;
 
@@ -309,14 +349,14 @@ public class DialogueManager : MonoBehaviour
     {
         Sequence sequence = DOTween.Sequence();
 
-        m_nextButtonObj.SetActive(false);
-        sequence.Append(m_dialogueContent.GetComponent<Image>().DOFade(0, transitionDuration).SetEase(m_fadeCurve));
-        sequence.Join(m_dialogueText.GetComponent<TMP_Text>().DOFade(0, transitionDuration).SetEase(m_fadeCurve));
-        if (m_textType == DialogueData.TextType.dialogue)
-        {
-            sequence.Join(m_talkerImage.GetComponent<Image>().DOFade(0, transitionDuration).SetEase(m_fadeCurve));
-            sequence.Join(m_talkerNameText.GetComponent<TMP_Text>().DOFade(0, transitionDuration).SetEase(m_fadeCurve));
-        }
+        m_nextButtonGFXObj.SetActive(false);
+        sequence.Append(m_dialogueMainContainerCanvasGroup.DOFade(0, transitionDuration).From(1).SetEase(m_fadeCurve));
+        //sequence.Join(m_dialogueText.GetComponent<TMP_Text>().DOFade(0, transitionDuration).SetEase(m_fadeCurve));
+        //if (m_textType == DialogueData.TextType.dialogue)
+        //{
+        //    sequence.Join(m_talkerImage.GetComponent<Image>().DOFade(0, transitionDuration).SetEase(m_fadeCurve));
+        //    sequence.Join(m_talkerNameText.GetComponent<TMP_Text>().DOFade(0, transitionDuration).SetEase(m_fadeCurve));
+        //}
 
         sequence.OnComplete(() =>
         {
@@ -327,7 +367,7 @@ public class DialogueManager : MonoBehaviour
             //    setImageColorAlpha(m_talkerImage.gameObject, 1);
             //    setTextColorAlpha(m_talkerNameText, 1);
             //}
-            m_dialogueContent.SetActive(false);
+            m_dialogueCanvas.SetActive(false);
             m_haveFinishDialogue = true;
         });
 
@@ -336,17 +376,18 @@ public class DialogueManager : MonoBehaviour
 
     private void DialogueZoomOut(float transitionDuration)
     {
-        Vector2 originPos = m_dialogueContent.GetComponent<RectTransform>().localScale;
+        Vector2 originPos = m_dialogueCanvas.GetComponent<RectTransform>().localScale;
 
-        m_dialogueContent.SetActive(true);
+        m_dialogueCanvas.SetActive(true);
         showNPCImage(true);
         Sequence sequence = DOTween.Sequence();
 
-        sequence.Append(m_dialogueContent.GetComponent<RectTransform>().DOScale(new Vector2(0, 0), transitionDuration).SetEase(m_fadeCurve));
+        sequence.Append(m_dialogueCanvas.GetComponent<RectTransform>().DOScale(new Vector2(0, 0), transitionDuration).SetEase(m_fadeCurve));
         sequence.OnComplete(() =>
         {
-            m_dialogueContent.SetActive(false);
-            m_dialogueContent.GetComponent<RectTransform>().localScale = originPos;
+            ToggleDialogueInterfaceElements(false);
+            m_dialogueCanvas.SetActive(false);
+            m_dialogueCanvas.GetComponent<RectTransform>().localScale = originPos;
             m_haveFinishDialogue = true;
         });
         sequence.Play();
@@ -358,8 +399,8 @@ public class DialogueManager : MonoBehaviour
         Vector2 originPos;
         Vector2 targetPos;
 
-        originPos.y = m_dialogueContent.transform.position.y;
-        originPos.x = m_dialogueContent.transform.position.x;
+        originPos.y = m_dialogueCanvas.transform.position.y;
+        originPos.x = m_dialogueCanvas.transform.position.x;
         targetPos = originPos;
 
         switch (swipOutDirection)
@@ -386,19 +427,19 @@ public class DialogueManager : MonoBehaviour
         }
         if (swipOutDirection == DialogueData.SwipOutDirection.toDown)
         {
-            m_dialogueContent.transform.DOMoveY(targetPos.y, transitionDuration).SetEase(m_swipeCurve).OnComplete(() =>
+            m_dialogueCanvas.transform.DOMoveY(targetPos.y, transitionDuration).SetEase(m_swipeCurve).OnComplete(() =>
             {
-                m_dialogueContent.transform.position = originPos;
-                m_dialogueContent.SetActive(false);
+                m_dialogueCanvas.transform.position = originPos;
+                m_dialogueCanvas.SetActive(false);
                 m_haveFinishDialogue = true;
             });
         }
         else if (swipOutDirection == DialogueData.SwipOutDirection.toLeft || swipOutDirection == DialogueData.SwipOutDirection.toRight)
         {
-            m_dialogueContent.transform.DOMoveX(targetPos.x, transitionDuration).SetEase(m_swipeCurve).OnComplete(() =>
+            m_dialogueCanvas.transform.DOMoveX(targetPos.x, transitionDuration).SetEase(m_swipeCurve).OnComplete(() =>
             {
-                m_dialogueContent.transform.position = originPos;
-                m_dialogueContent.SetActive(false);
+                m_dialogueCanvas.transform.position = originPos;
+                m_dialogueCanvas.SetActive(false);
                 m_haveFinishDialogue = true;
             });
         }
@@ -409,7 +450,7 @@ public class DialogueManager : MonoBehaviour
     {
         if (m_textType == DialogueData.TextType.dialogue)
         {
-            m_talkerImage.gameObject.SetActive(isShown);
+            //m_talkerImage.gameObject.SetActive(isShown);
             //npcNameGO.SetActive(isShown);
             m_talkerNameText.gameObject.SetActive(isShown);
         }
