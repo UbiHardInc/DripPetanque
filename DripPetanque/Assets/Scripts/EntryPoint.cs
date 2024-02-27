5ba5c514db54149a5c8391f4a450eae7bc763109
p.luneau <paul.luneau@mail.com>
2024-02-27 12:24:13 +0100

    Entry point and scene

---
+++ DripPetanque/Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EntryPoint : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset m_asset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_asset.Enable();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
