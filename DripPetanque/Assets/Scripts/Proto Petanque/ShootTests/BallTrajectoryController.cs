b0f9f8b2309cc435f09ab978c78d41a879449f41
p.luneau <paul.luneau@mail.com>
2024-02-27 11:07:34 +0100

    Moved scripts

--- DripPetanque/Assets/Scripts/Proto Petanque/ShootTests/BallTrajectoryController.cs
+++ DripPetanque/Assets/Scripts/Proto Petanque/ShootTests/BallTrajectoryController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
+using UnityUtility.Pools;
 
 public class BallTrajectoryController : MonoBehaviour
 {
+    [SerializeField] private RigidbodyPool m_ballPool;
     [SerializeField] private Rigidbody m_ball;
     [SerializeField] private GameObject m_floor;
     [SerializeField] private SplineContainer m_spline;
@@ -15,7 +17,8 @@ public class BallTrajectoryController : MonoBehaviour
     [SerializeField, Range(0.0f, 1.0f)] private float m_releaseBallProgress = 0.95f;
     [SerializeField] private float m_releaseForce = 10.0f;
 
-    [NonSerialized] private Rigidbody m_currentBall = null;
+    [NonSerialized] private Rigidbody m_currentBall = default;
+    [NonSerialized] private List<PooledObject<Rigidbody>> m_allBalls = new List<PooledObject<Rigidbody>>();
 
     [NonSerialized] private bool m_ballOnSpline = false;
     [NonSerialized] private float m_ballProgress = 0.0f;
@@ -25,6 +28,7 @@ public class BallTrajectoryController : MonoBehaviour
     private void Start()
     {
         m_ballOnSpline = false;
+
     }
 
     private void FixedUpdate()
@@ -37,7 +41,7 @@ public class BallTrajectoryController : MonoBehaviour
                 return;
             }
 
-            m_ballProgress += m_speedAlongTheSpline.Evaluate(m_ballProgress) * Time.fixedDeltaTime * m_speedFactor;
+            m_ballProgress += m_speedAlongTheSpline.Evaluate(m_ballProgress) * Time.fixedDeltaTime * m_speedFactor / m_spline.CalculateLength();
             m_currentBall.transform.position = m_spline.EvaluatePosition(m_ballProgress);
         }
     }
@@ -52,8 +56,10 @@ public class BallTrajectoryController : MonoBehaviour
         m_ballOnSpline = true;
         m_ballProgress = 0.0f;
 
-        m_currentBall = Instantiate(m_ball);
-        //m_currentBall.collisionDetectionMode = m_ball.collisionDetectionMode;
+        PooledObject<Rigidbody> requestedBall = m_ballPool.Request();
+        m_allBalls.Add(requestedBall);
+        m_currentBall = requestedBall.Object;
+
         m_currentBall.transform.position = m_spline.EvaluatePosition(0);
 
 
@@ -61,18 +67,20 @@ public class BallTrajectoryController : MonoBehaviour
         m_currentBall.gameObject.SetActive(true);
     }
 
-    public IEnumerator ClearAllBalls()
+    public void ClearAllBalls()
     {
-        m_floor.SetActive(false);
-        yield return new WaitForSeconds(2.0f);
-        m_floor.SetActive(true);
+        foreach (PooledObject<Rigidbody> requestedBall in m_allBalls)
+        {
+            requestedBall.Release();
+        }
+        m_allBalls.Clear();
     }
 
     private void ReleaseBall()
     {
         m_ballOnSpline = false;
 
-        Vector3 ballForward = m_spline.EvaluateTangent(m_ballProgress);
+        Vector3 ballForward = ((Vector3)m_spline.EvaluateTangent(m_ballProgress)).normalized;
         float ballSpeed = m_speedAlongTheSpline.Evaluate(m_ballProgress);
 
         m_currentBall.isKinematic = false;
--- DripPetanque/Assets/Scripts/Proto Petanque/ShootTests/CustomSplineController.cs
+++ DripPetanque/Assets/Scripts/Proto Petanque/ShootTests/CustomSplineController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
+using UnityUtility.Utils;
 
 public class CustomSplineController : MonoBehaviour
 {
@@ -16,7 +17,8 @@ public class CustomSplineController : MonoBehaviour
     [SerializeField, Range(0.0001f, 0.9999f)] private float m_forwardFactor = 0.5f;
     [SerializeField, Range(0.0f, 2.0f)] private float m_attractionFactor = 0.0f;
 
-
+    [SerializeField, Min(0.0f)] private float m_forcePow = 1.0f;
+    [SerializeField, Min(0.0f)] private float m_forceMult = 1.0f;
 
     [SerializeField] private SplineContainer m_splineComponent = null;
 
@@ -29,6 +31,7 @@ public class CustomSplineController : MonoBehaviour
     // Gizmos
     [NonSerialized] private Vector3 m_attractionPointPosition = Vector3.zero;
 
+    [ContextMenu(nameof(Start))]
     private void Start()
     {
         m_spline = m_splineComponent.Spline;
@@ -42,11 +45,12 @@ public class CustomSplineController : MonoBehaviour
 
     private void Update()
     {
-        UpdateSpline();
+        //UpdateSpline();
     }
 
-    private void UpdateSpline()
+    private void OldMethod()
     {
+
         Vector3 startPosition = m_startPoint.position;
         Vector3 endPosition = m_endPoint.position;
 
@@ -57,6 +61,14 @@ public class CustomSplineController : MonoBehaviour
                            Vector3.Cross(startToEnd, Vector3.up).normalized * Mathf.Sin(m_angle * Mathf.Deg2Rad);
 
         Vector3 attractionPoint = attractionPointProj + splineUp * m_height;
+        UpdateSpline(attractionPoint);
+    }
+
+    private void UpdateSpline(Vector3 attractionPoint)
+    {
+        Vector3 startPosition = m_startPoint.position;
+        Vector3 endPosition = m_endPoint.position;
+
         m_attractionPointPosition = attractionPoint;
 
         m_firstKnot.Position = startPosition;
@@ -68,6 +80,18 @@ public class CustomSplineController : MonoBehaviour
 
         m_spline.SetKnot(0, m_firstKnot);
         m_spline.SetKnot(1, m_lastKnot);
+    }
+
+    public void SetSplineParameters(float yAngle, float xAngle, float force)
+    {
+        float forceMagnitude = Mathf.Pow(force, m_forcePow) * m_forceMult;
+        Vector3 splineForward = transform.forward.Rotate(transform.up, yAngle);
+        Vector3 attractionPoint = splineForward.Rotate(-Vector3.Cross(splineForward, transform.up), xAngle).normalized * forceMagnitude;
+        attractionPoint = m_startPoint.position + attractionPoint;
+
+        m_endPoint.position = attractionPoint.ProjectOn(splineForward) / m_forwardFactor;
+
+        UpdateSpline(attractionPoint);
     }
 
 #if UNITY_EDITOR
