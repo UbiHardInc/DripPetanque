7f8bb34622f09b486c2f9ff139f22a0321775bba
p.luneau <paul.luneau@mail.com>
2024-02-29 17:52:27 +0100

    Added PetanqueGameSettings scriptable

---
+++ DripPetanque/Assets/Scripts/Petanque/PetanqueGameSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(PetanqueGameSettings), menuName = "Scriptables/" + nameof(PetanqueGameSettings))]
+public class PetanqueGameSettings : ScriptableObject
+{
+    public int BallsPerGame => m_ballsPerGame;
+    public int PointsToWin => m_pointsToWin;
+
+    [SerializeField] private int m_ballsPerGame = 3;
+    [SerializeField] private int m_pointsToWin = 13;
+}
