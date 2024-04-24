d08c7f37e5bdcc9287fd53c6850049e20bb6c9e4
p.luneau <paul.luneau@mail.com>
2024-04-24 19:33:07 +0200

    Filled the DialogueSubGameManager + added an event when a dialogue ends + fixed format

--- DripPetanque/Assets/Scripts/GameManagers/DialogueSubGameManager.cs
+++ DripPetanque/Assets/Scripts/GameManagers/DialogueSubGameManager.cs
@@ -1,4 +1,43 @@
+using System;
+using UnityEngine;
+
 public class DialogueSubGameManager : SubGameManager
 {
     public override GameState CorrespondingState => GameState.Dialogue;
+
+    [SerializeField] private DialogueManager m_dialogueManager;
+
+    [NonSerialized] private DialogueData m_dialogueToStart;
+
+
+    public override void BeginState(GameState previousState)
+    {
+        base.BeginState(previousState);
+
+        m_dialogueToStart = m_sharedDatas.NextDialogueToStart;
+        if (m_dialogueToStart == null)
+        {
+            Debug.LogError($"No {nameof(DialogueData)} in the shared datas : Exiting the Dialogue state");
+            m_requestedGameState = previousState;
+            return;
+        }
+        m_sharedDatas.NextDialogueToStart = null;
+
+        m_dialogueManager.OnDialogueEnded += OnDialogueEnded;
+        m_dialogueManager.StartDialogue(m_dialogueToStart);
+    }
+
+    private void OnDialogueEnded()
+    {
+        m_dialogueManager.OnDialogueEnded -= OnDialogueEnded;
+
+        if (m_dialogueToStart.NextGameState == GameState.None)
+        {
+            Debug.LogError($"The next game state of the current dialogue ({m_dialogueToStart.name}) is equals to {GameState.None}\nCan't exit the current game state");
+            return;
+        }
+
+        m_requestedGameState = m_dialogueToStart.NextGameState;
+        m_dialogueToStart = null;
+    }
 }
