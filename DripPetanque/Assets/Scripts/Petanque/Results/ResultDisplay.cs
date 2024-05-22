f26b467e16543d5f3277e75854f038ad9943375a
p.luneau <paul.luneau@mail.com>
2024-05-23 00:00:49 +0200

    Better ResultDisplay

--- DripPetanque/Assets/Scripts/Petanque/Results/ResultDisplay.cs
+++ DripPetanque/Assets/Scripts/Petanque/Results/ResultDisplay.cs
@@ -1,21 +1,31 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ResultDisplay : MonoBehaviour
 {
     [SerializeField] private InputActionReference m_endResultInput;
+    [SerializeField] private TMP_Text m_text;
 
     [NonSerialized] private Action m_onDisplayResultEnds;
 
-    public void DislayResult(Action onDisplayResultEnds)
+    public void Init()
     {
+        gameObject.SetActive(false);
+    }
+
+    public void DislayResult(ResultDatas result, Action onDisplayResultEnds)
+    {
+        gameObject.SetActive(true);
+        m_text.text = $"{result.Winner} won the game with {result.Points} points";
         m_onDisplayResultEnds = onDisplayResultEnds;
         m_endResultInput.action.performed += OnEndResultInputPerformed;
     }
 
     private void OnEndResultInputPerformed(InputAction.CallbackContext context)
     {
+        gameObject.SetActive(false);
         m_endResultInput.action.performed -= OnEndResultInputPerformed;
         m_onDisplayResultEnds();
     }
