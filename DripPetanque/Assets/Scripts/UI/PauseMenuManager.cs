ff6a233fb3e69f8e2635af58a66bf9c5c3ab2ad5
p.luneau <paul.luneau@mail.com>
2024-06-08 17:34:49 +0200

    Removed m_isOnUI and unsubscibed the PauseMenu from the input events when the pause menu is closed

--- DripPetanque/Assets/Scripts/UI/PauseMenuManager.cs
+++ DripPetanque/Assets/Scripts/UI/PauseMenuManager.cs
@@ -23,8 +23,10 @@ public class PauseMenuManager : MonoBehaviour
     [SerializeField] private InputActionReference m_submitInput;
     [SerializeField] private InputActionReference m_cancelInput;
 
+    [Title("Misc")]
+    [SerializeField] private float m_sliderStep = 0.1f;
+
     //private variables
-    private bool m_isOnUI;
 
     [NonSerialized] private GameManager m_gameManager;
 
@@ -33,13 +35,6 @@ public class PauseMenuManager : MonoBehaviour
     protected void Start()
     {
         m_gameManager = GameManager.Instance;
-
-        m_openMenuInput.action.performed += OpenMenu;
-        m_closeMenuInput.action.performed += CloseMenu;
-        m_moveInput.action.started += MoveInUI;
-        m_submitInput.action.performed += PressSubmit;
-        m_cancelInput.action.performed += PressCancel;
-
     }
 
     #region InputActionsMethods
@@ -57,26 +52,17 @@ public class PauseMenuManager : MonoBehaviour
 
     private void PressCancel(InputAction.CallbackContext obj)
     {
-        if (m_isOnUI)
-        {
-            SoundManager.Instance.PlayUISFX("cancel");
-        }
+        SoundManager.Instance.PlayUISFX("cancel");
     }
 
     private void PressSubmit(InputAction.CallbackContext obj)
     {
-        if (m_isOnUI)
-        {
-            //SoundManager.Instance.PlayUISFX("submit");
-        }
+        //SoundManager.Instance.PlayUISFX("submit");
     }
 
     private void MoveInUI(InputAction.CallbackContext obj)
     {
-        if (m_isOnUI)
-        {
-            SoundManager.Instance.PlayUISFX("move");
-        }
+        SoundManager.Instance.PlayUISFX("move");
     }
     #endregion
 
@@ -84,7 +70,7 @@ public class PauseMenuManager : MonoBehaviour
 
     public void OpenPauseMenu(bool fromMainMenu)
     {
-        //Debug.LogError("OpenPauseMenuCalled");
+        SubscribeToInputEvents();
         m_pauseMenu.SetActive(true);
 
         if (!fromMainMenu)
@@ -96,7 +82,6 @@ public class PauseMenuManager : MonoBehaviour
         else
         {
             SoundManager.Instance.PlayUISFX("submit");
-            m_isOnUI = true;
             m_backMainMenuButton.gameObject.SetActive(false);
         }
         m_firstSelectedButton.Select();
@@ -105,6 +90,7 @@ public class PauseMenuManager : MonoBehaviour
 
     public void ClosePauseMenu()
     {
+        UnsubscribeToInputEvents();
         m_pauseMenu.SetActive(false);
         if (m_gameManager.CurrentSubGameManager.CorrespondingState == GameState.MainMenu)
         {
@@ -112,39 +98,57 @@ public class PauseMenuManager : MonoBehaviour
         }
         else
         {
-            m_isOnUI = false;
             Time.timeScale = 1f;
         }
 
     }
 
+    // Called by buttons
     public void MusicVolumeChange(bool add)
     {
         if (add)
         {
-            m_musicSlider.value += 0.1f;
+            m_musicSlider.value += m_sliderStep;
         }
         else
         {
-            m_musicSlider.value -= 0.1f;
+            m_musicSlider.value -= m_sliderStep;
         }
         _ = m_musicMixerGroup.audioMixer.SetFloat("musicVol", Mathf.Log10(m_musicSlider.value) * 20);
 
     }
 
+    // Called by buttons
     public void SfxVolumeChange(bool add)
     {
         if (add)
         {
-            m_sfxSlider.value += 0.1f;
+            m_sfxSlider.value += m_sliderStep;
         }
         else
         {
-            m_sfxSlider.value -= 0.1f;
+            m_sfxSlider.value -= m_sliderStep;
         }
         _ = m_sfxMixerGroup.audioMixer.SetFloat("sfxVol", Mathf.Log10(m_sfxSlider.value) * 20);
         SoundManager.Instance.PlayBallSounds(SoundManager.BallSFXType.swoop);
+    }
 
+    private void SubscribeToInputEvents()
+    {
+        m_openMenuInput.action.performed += OpenMenu;
+        m_closeMenuInput.action.performed += CloseMenu;
+        m_moveInput.action.started += MoveInUI;
+        m_submitInput.action.performed += PressSubmit;
+        m_cancelInput.action.performed += PressCancel;
+    }
+
+    private void UnsubscribeToInputEvents()
+    {
+        m_openMenuInput.action.performed -= OpenMenu;
+        m_closeMenuInput.action.performed -= CloseMenu;
+        m_moveInput.action.started -= MoveInUI;
+        m_submitInput.action.performed -= PressSubmit;
+        m_cancelInput.action.performed -= PressCancel;
     }
 
     #endregion
